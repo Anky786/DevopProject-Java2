55ffe824ea552356c6056e1f34d96addd789b7d6
Anky786 <ankit.potdukhe@gmail.com>
2024-08-26 18:09:47 +0530

    Update Hello.java

--- src/main/java/se/kth/compilers/Hello.java
+++ src/main/java/se/kth/compilers/Hello.java
@@ -6,6 +6,6 @@ public class Hello {
       System.out.printf("Hello %s!", args[0]);
       return;
     }
-    System.out.print("Hello Anky Assasinmncncs-ok-87");
+    System.out.print("Hello Anky Assbggfygfvgfgvgasinmncncs-ok-87");
   }
 }
